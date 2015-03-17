31817702f54896d2c8549bd20a2d1fded1989b97
Aaron <aaronramshaw@gmail.com>
2015-03-17 21:47:41 +1100

    script work

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 function amIHome() {
 	var home;
-   $.get("http://60.241.171.39", function( home ) {
+   $.get("http://60.241.171.39", function( home ), html){
     // my_var contains whatever that request returned
 }, 'html');  // or 'text', 'xml', 'more'
    alert(home);
