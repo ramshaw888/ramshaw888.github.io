de8abfc35c8eba029f43dbc3c5c626d9e490768c
Aaron <aaronramshaw@gmail.com>
2015-03-17 22:31:40 +1100

    working

---
+++ home.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+
+htmlHeader = '''Content-type: text/html; charset=utf-8\nCache-Control: no-cache, no-store, must-revalidate\nPragma: no-cache\nExpires: 0\n\n
+'''
+htmlBody = '''home'''
+
+print htmlHeader + htmlBody
--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 function amIHome() {
-	$.get("demo_test.asp", function(data, status){
-            alert("Data: " + data + "\nStatus: " + status);
+	$.get("http://60.241.171.39/index.cgi", function(data, status){
+		if ( data == "\nhome\n" ) {
+			document.getElementById("home_check").innerHTML = "yes, I am at home now.";
+		}
+		else {
+			document.getElementById("home_check").innerHTML = "no, I am not at home now.";
+		}
+
     });
+
 }
