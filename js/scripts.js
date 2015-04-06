1f58dad3f5b41c6d35bdb7767233d8f60db04e28
Aaron <aaronramshaw@gmail.com>
2015-04-06 22:55:48 +1000

    fixed home status

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 function amIHome() {
 	$.get("http://60.241.171.39/index.cgi", function(data, status){
 		if ( data == "\nhome\n" ) {
-			document.getElementById("home_check").innerHTML = "yes, I am at home now.";
+			document.getElementById("home_check").innerHTML = "I am at home now.";
 		}
 		else {
-			document.getElementById("home_check").innerHTML = "no, I am not at home now.";
+			document.getElementById("home_check").innerHTML = "I am not at home now.";
 		}
 
     });
